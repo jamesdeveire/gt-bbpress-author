39b037cea9cff8d38c7060ebb0a761e16bc36400
David Chandra Purnama <david@turtlepod.org>
2012-09-27 01:56:15 -0700

    first

---
+++ gt-bbress-author.php
@@ -0,0 +1,89 @@
+<?php
+/**
+ * Plugin Name: GT | bbPress Author Drop Down
+ * Plugin URI: http://genbutheme.com/
+ * Description: Enable author drop down in BBPress admin to change user for topics and reply.
+ * Version: 0.1
+ * Author: David Chandra Purnama
+ * Author URI: http://shellcreeper.com/
+ *
+ * Sometimes you need to change the author of topic and reply in BBPress,
+ * this plugin will add an author metabox in topic and reply edit screen
+ * in WordPress admin so you can select topic or reply poster from all registered member.
+ *
+ * This program is free software; you can redistribute it and/or modify it under the terms  
+ * of the GNU General Public License version 2, as published by the Free Software Foundation.
+ * You may NOT assume that you can use any other version of the GPL.
+ * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; 
+ * without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ *
+ * @version 0.1
+ * @author David Chandra Purnama <david@turtlepod.org>
+ * @copyright Copyright (c) 2012, David Chandra Purnama
+ * @license http://www.gnu.org/licenses/old-licenses/gpl-2.0.html
+ */
+
+/* hook post type feature to init, as recomendation in codex */
+add_action( 'init', 'gt_bbpress_add_author' );
+
+/**
+ * Add Author Feature in BBPress Topics and Replay
+ * @since 0.1
+ */
+function gt_bbpress_add_author() {
+
+	/* add author support for post type "topic" and "reply" */
+	add_post_type_support( 'topic', 'author' );
+	add_post_type_support( 'reply', 'author' );
+}
+
+
+/* filter to change drop down user in author metabox */
+add_filter('wp_dropdown_users', 'gt_bbpress_author_metabox');
+
+/**
+ * Add all member in Author Meta Box
+ * as default WordPress only add member with "author" capability
+ * in author metabox
+ *
+ * BBPress use function "bbp_get_topic_post_type" and "bbp_get_reply_post_type"
+ * to determined post type used as topic and reply, ideally this should be used as
+ * conditional check. 
+ * we wont use that,and set it directly to "topic" and "reply"
+ *
+ * The one we are filtering is 'post_author_meta_box'
+ * defined in wp-admin\includes\meta-boxes.php
+ *
+ * @since 0.1
+ */
+function gt_bbpress_author_metabox( $output ){
+
+	/* globalize post and post type object. */
+	global $post,$post_type;
+
+	/* conditional check if it's a topic or reply post type, 
+	 * so it only apply in replay and topic edit screen
+	 */
+	if ( is_admin() && ($post_type == 'topic' ||  $post_type == 'reply') ) {
+
+		/* get all user. */
+		$users = get_users();
+
+		/* start with select output */
+		$output = '<select id="post_author_override" name="post_author_override" class="">';
+
+		/* for each user create an option drop down */
+		foreach($users as $user){
+
+			/* get topic/reply submitter as selected author */
+			$selected = ( $post->post_author == $user->ID )?"selected='selected'":'';
+
+			/*  create the options drop down  */
+			$output .= '<option value="'.$user->ID.'"'.$selected.'>'.$user->user_login.'</option>';
+		}
+		/* wrap it up. */
+		$output .= "</select>";
+	}
+	/* and return the output. */
+	return $output;
+}
